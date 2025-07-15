e63e0e68a5998f49dbcc3579c9ed883bb79d7082
mdfazil <fazilsaifi250@gmail.com>
2025-07-15 16:51:40 +0530

    Integrate frontend build with backend

--- server/server.js
+++ server/server.js
@@ -37,13 +37,3 @@ app.use((req, res) => {
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-const path = require('path');
-
-// Serve frontend static files
-app.use(express.static(path.join(__dirname, 'fronten/build')));
-
-// Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'fronten/build', 'index.html'));
-});
-
